718ac949ca867c618cb727590547feddd2676c3d
MendezV <jf.mendez53@uniandes.edu.co>
2019-04-12 16:17:09 -0400

    changes that dont matter

--- test_CHOH2/myscript.sh
+++ test_CHOH2/myscript.sh
@@ -10,9 +10,5 @@
 declare -a commands
 
 commands[1]="jdftx -i Neutral.in -o Neutral.out"
-commands[2]="for iMu in {9..10}; do
-    export mu="$(echo $iMu | awk '{printf("%.4f", -0.2015+0.1*$1/27.2114)}')"
-    mpirun -n 4 jdftx -i Charged.in | tee Charged$mu.out
-    mv common.nbound Charged$mu.nbound
-done"
+
 bash -c "${commands[${SLURM_ARRAY_TASK_ID}]}"
