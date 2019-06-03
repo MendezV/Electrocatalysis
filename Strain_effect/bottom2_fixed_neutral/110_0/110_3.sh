30e7eca8f2d42e5e169f9d13d6e23322f3a9bc81
MendezV <jf.mendez53@uniandes.edu.co>
2019-06-03 19:44:09 -0400

    results

---
+++ Strain_effect/bottom2_fixed_neutral/110_0/110_3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=nodes
+#SBATCH --job-name=Pt_Surf110_3_solv
+#SBATCH --output=Pt_Surf110_3_solv_%a.txt
+#SBATCH --array=1-2
+#SBATCH --exclude=node[1001-1032]
+#SBATCH --export=surface=110_3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=12
+
+
+declare -a commands
+
+commands[1]="/home/jfm343/JDFTXDIR/build/jdftx -i Vacuum.in | tee ${surface}-Vacuum.out; /home/jfm343/JDFTXDIR/build/jdftx -i Solvated.in | tee ${surface}-Solvated.out"
+commands[2]="echo done"
+bash -c "${commands[${SLURM_ARRAY_TASK_ID}]}"
